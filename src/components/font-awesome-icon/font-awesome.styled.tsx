002aaffe088aa42e7c80936e4dfc5a6175db0573
Shahriaz Kabir <34005640+sshahriazz@users.noreply.github.com>
2022-07-22 20:06:28 +0600

    Icon Implemented,trash moved separately

--- src/components/font-awesome-icon/font-awesome.styled.tsx
+++ src/components/font-awesome-icon/font-awesome.styled.tsx
@@ -6,4 +6,27 @@ export const FontAwesome = styled(FontAwesomeIcon, {
   py: "0",
   mx: "0",
   my: "0",
+  variants: {
+    size: {
+      base: { fontSize: "$base" },
+      sm: { fontSize: "$sm" },
+      md: { fontSize: "$md" },
+      lg: { fontSize: "$lg" },
+      xl: { fontSize: "$xl" },
+      xl2: { fontSize: "$xl2" },
+    },
+    color: {
+      current: { color: "currentColor" },
+      base: { color: "$accents8" },
+      dark: { color: "$accents9" },
+      primary: { color: "$primary" },
+      secondary: { color: "$secondary" },
+      success: { color: "$success" },
+      error: { color: "$error" },
+    },
+  },
+  defaultVariants: {
+    size: "md",
+    color: "current",
+  },
 });
